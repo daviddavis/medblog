4fa1c96e6828308d9dd96304715b4395fb603491
David Davis <davidd@scimedsolutions.com>
2011-02-20 20:20:57 -0500

    Checking for spelling errors and saving them in session

--- lib/rack/spellcheck.rb
+++ lib/rack/spellcheck.rb
@@ -1,13 +1,49 @@
 module Rack
   class Spellcheck
-    def initialize(app)
+
+    DICTIONARIES = "doc/en_US,doc/en_US_OpenMedSpel"
+
+    def initialize(app, ignored_fields = nil)
       @app = app
+      @ignored_fields ||= ["utf8", "authenticity_token", "commit"]
     end
 
     def call(env)
+      req = Rack::Request.new(env)
+      if req.post? || req.put? # create or update
+        mispellings = req.params.dup.reject {|key, val| @ignored_fields.include?(key.to_s)}
+        mispellings = check_params(mispellings)
+        req.session[:mispellings] = mispellings
+        puts "Set mispellings in session to: #{mispellings}"
+      end
+
       status, headers, response = @app.call(env)
-      puts "Inside Spellcheck"
       [status, headers, response]
     end
+
+    def check_params(params)
+      params.each do |(key, val)|
+        if val.is_a?(String)
+          params[key] = check(val)
+        elsif val.is_a?(Hash)
+          params[key] = check_params(val.clone)
+        else
+          raise "Don't know how to handle #{val.class.name}"
+        end
+      end
+    end
+
+    def check(string)
+      mispellings = []
+      IO.popen("hunspell -l -d #{DICTIONARIES}", "w+") do |f|
+        f.puts string
+        f.close_write
+        while(output = f.gets)
+          mispellings << output.strip
+        end
+      end
+      return mispellings
+    end
+
   end
 end
